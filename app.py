0fe769269acffe2250b13c16f2479f4c1ba843f8
alvsgabriellk <cauagabriell81@gmail.com>
2025-11-05 11:59:07 -0300

    Update completed in app.py of Update - Crud

--- app.py
+++ app.py
@@ -15,28 +15,47 @@ def create_tasks():
     print(tasks)
     return jsonify({"message": "Nova tarefa criada com sucesso"}) # resposta da API
 
+# R - CRUD / READ //// LENDO TODOS OS DADOS
 @app.route('/tasks', methods=['GET']) # ta requisitando os dados
 def get_tasks():
     #metodo simples e menos visual
-    task_list = [task.to_dict() for task in tasks]
+    task_list = [task.to_dict() for task in tasks] # QUANDO CHAMAR, VAI SAIR COMO DICIONARIO DA CLASSE
     
     #metodo visual e complexo
     for task in tasks:
         task_list.append(task.to_dict())
     outpot = {
-        "tasks": task_list,
-        "total_tasks": len(task_list)
+        "tasks": task_list, # VAI CHAMAR O DICIONARIO DA TAREFA PRA CA, A EXIBIÇÃO
+        "total_tasks": len(task_list) 
         }
-    return jsonify(outpot)
+    return jsonify(outpot) # RETORNAR EM JSON PRA A API
 
+# R - CRUD / READ //// LENDO APENAS UM DADO EM ESPECIFICO
 @app.route('/tasks/<int:id>', methods=['GET'])
 def get_task(id):
     task = None
     for t in tasks:
         if t.id == id:
-            return jsonify(t.to_dict())
+            return jsonify(t.to_dict()) # VAI RETORNAR EM JSON PRA API A TAREFA CHAMADA EM DICIONARIO
         
     return jsonify({"message": "Não foi possível encontrar a atividade."}), 404
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+    print(task)
+    if task == None:
+        return jsonify({"message": "Não foi possível encontrar a atividade."}), 404
+    
+    data = request.get_json()
+    task.title = data['titulo']
+    task.descricao = data['descricao']
+    task.completo = data['completo']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
 
 if __name__ == "__main__":
     app.run(debug=True)
